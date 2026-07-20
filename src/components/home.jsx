a6ab15a75571f2fe4ef51b214455603d9d79b7ab
Srikanth46148346 <117381627+Srikanth46148346@users.noreply.github.com>
2026-07-20 21:34:05 +0530

    Initial files

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,31 +1,29 @@
 import React from "react";
 import Child from "./child";
 import Test from "./test";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
+function Home() {
+  const some = ["sdf", "df", "23sd", "v323", "sdf2", "98sdf", "sdfb"];
 
-function Home(){
+  const navigate = useNavigate();
 
-    const some = [ 'sdf', 'df', '23sd', 'v323', 'sdf2', '98sdf', 'sdfb'];
-
-    const navigate = useNavigate();
-
-    function handleNavigation(){
-        navigate('/test');
-    }
-    return(
-        <div>
-            <h3>Hello I'm from Home component</h3>
-            <ul>
-                {some.map((val, index) =>(
-                    <li key={index}>{val}</li>
-                ) )}
-            </ul>
-            <Child some={some} />
-            {/* <Test /> */}
-            <button onClick={handleNavigation}>Navigate</button>
-        </div>
-    )
+  function handleNavigation() {
+    navigate("/test");
+  }
+  return (
+    <div>
+      <h3>Hello I'm from Home component</h3>
+      <ul>
+        {some.map((val, index) => (
+          <li key={index}>{val}</li>
+        ))}
+      </ul>
+      <Child some={some} />
+      {/* <Test /> */}
+      <button onClick={handleNavigation}>Navigate</button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
